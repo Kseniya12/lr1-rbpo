5a76e3bfdb1c77151f97ad248bf84e7e69baa226
Kseniya12 <90384995+Kseniya12@users.noreply.github.com>
2022-01-20 22:01:54 +0300

    Update newlabrab1.cpp

--- newlabrab1.cpp
+++ newlabrab1.cpp
@@ -1,59 +1,62 @@
-﻿#include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
-unsigned short personHeight;
+unsigned personHeight;
 unsigned short ReadPersonAge() {
-    unsigned short age;
-    cout << "age = ";
-    cin >> age;
-    return age;
+	unsigned short age;
+	cout << "Age:";
+	cin >> age;
+	return age;
 }
 string ReadPersonName() {
-    string name; 
-    cout << "name = ";
-    cin >> name;
-    return name;
+	string Name;
+	cout << "Name:";
+	cin >> Name;
+	return Name;
 }
-
 void ReadPersonHeight() {
-    cout << "height =";
-    cin >> personHeight;
+	cout << "Height:";
+	cin >> personHeight;
 }
-
-void ReadPersonWeight(unsigned short& weight) {
-    cout << "weight = ";
-    cin >> weight;
+void ReadPersonWeight(unsigned short &weight) {
+	cout << "Weight:";
+	cin >> weight;
 }
-
-void ReadPersonSalary(double* salary) {
-    cout << "salary = ";
-    cin >> *salary;
+void ReadPersonSalary(double* Salary) {
+	cout << "Salary:";
+	cin >> *Salary;
 }
-
-void ReadPersonData(string& name, unsigned short& age, string& salary) {
-    name = ReadPersonName();
-    age = ReadPersonAge();
-    ReadPersonSalary(&salary);
+void ReadPersonData(string& Name, unsigned short& Age, string& Salary) {
+	Name=ReadPersonName();
+	Age=ReadPersonAge();
+	double doubleSalary;
+	doubleSalary = static_cast<double>(strtoul(Salary.c_str(), NULL, 0));
+	ReadPersonSalary(&doubleSalary);
+	Salary = to_string(doubleSalary);
 }
-
-void ReadPersonData(string& name, unsigned short& age, string& height, string& weight) {
-    name = ReadPersonName();
-    age = ReadPersonAge();
-    ReadPersonHeight();
-    height = personHeight;
-    ReadPersonWeight(weight);
+void ReadPersonData(string& Name, unsigned short& Age, string& Height,string& Weight) {
+	Name=ReadPersonName();
+	Age=ReadPersonAge();
+	ReadPersonHeight();
+	Height = to_string(personHeight);
+	unsigned short ShortWeight;
+	ShortWeight = static_cast<unsigned short>(strtoul(Weight.c_str(), NULL, 0));
+	ReadPersonWeight(ShortWeight);
+	Weight = to_string(ShortWeight);
 }
-
-void WritePersonData(unsigned short age, string name, const string& height = "", const string& weight = "", const string& salary = "") {
-    cout << name << endl << age << endl << height << endl << weight << endl << salary << endl;
+void WritePersonData(unsigned short Age,const string& Name="", const string& Height="", const string& Weight="", const string& Salary="") {
+	cout << "PersonData:" << endl;
+	cout << "Name:" <<" "<< Name << endl;
+	cout << "Age" << " " << Age << endl;
+	cout << "Height" << " " << Height << endl;
+	cout << "Weight" << " " << Weight << endl;
+	cout << "Salary" << " " << Salary << endl;
 }
-
-int main() {
-    string name;
-    unsigned short age, height, weight;
-    ReadPersonData(name, age, salary);
-    WritePersonData(age, name, "", "", salary);
-    ReadPersonData(name, age, height, weight);
-    WritePersonData(age, name, height, weight);
-    return 0;
+void main() {
+	string Name, Height, Weight, Salary;
+	unsigned short Age;
+	ReadPersonData(Name, Age, Salary);
+	WritePersonData(Age, Name, "", "", Salary);
+	ReadPersonData(Name, Age, Height, Weight);
+	WritePersonData(Age, Name, Height, Weight, "");
 }
